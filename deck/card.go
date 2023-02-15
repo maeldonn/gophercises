99bf8d3cc0f3f4e0263a5a32369974db59a0d9e6
Maël DONNART <mael.donnart1@gmail.com>
2023-02-15 23:37:35 +0100

    WIP: Exercise #9: Deck of Cards

--- deck/card.go
+++ deck/card.go
@@ -2,7 +2,12 @@
 
 package deck
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
 
 type Suit uint8
 
@@ -52,12 +57,58 @@ func (c *Card) String() string {
 	return fmt.Sprintf("%s of %ss", c.Rank.String(), c.Suit.String())
 }
 
-func New() []Card {
+func New(opts ...func([]Card) []Card) []Card {
 	var cards []Card
 	for _, suit := range suits {
 		for rank := minRank; rank <= maxRank; rank++ {
 			cards = append(cards, Card{Suit: suit, Rank: rank})
 		}
 	}
+	for _, opts := range opts {
+		cards = opts(cards)
+	}
 	return cards
+}
+
+func DefaultSort(cards []Card) []Card {
+	sort.Slice(cards, Less(cards))
+	return cards
+}
+
+func Sort(less func(cards []Card) func(i, j int) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		sort.Slice(cards, less(cards))
+		return cards
+	}
+}
+
+func Less(cards []Card) func(i, j int) bool {
+	return func(i, j int) bool {
+		return absRank(cards[i]) < absRank(cards[j])
+	}
+}
+
+func absRank(c Card) int {
+	return int(c.Suit)*int(maxRank) + int(c.Rank)
+}
+
+func Shuffle(cards []Card) []Card {
+	shuffled := make([]Card, len(cards))
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for i, j := range r.Perm(len(cards)) {
+		shuffled[i] = cards[j]
+	}
+	return shuffled
+}
+
+func Jokers(n int) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		for i := 0; i < n; i++ {
+			cards = append(cards, Card{
+				Rank: Rank(i),
+				Suit: Joker,
+			})
+		}
+		return cards
+	}
 }
--- deck/card_test.go
+++ deck/card_test.go
@@ -28,3 +28,51 @@ func TestNew(t *testing.T) {
 		t.Errorf("Wrong numbers of cards in a new deck, got %d want %d", got, want)
 	}
 }
+
+func TestDefaultSort(t *testing.T) {
+	got := New(DefaultSort)[0]
+	want := Card{Rank: Ace, Suit: Spade}
+	if got != want {
+		t.Errorf("Expected %s as first card, got %s", want.String(), got.String())
+	}
+}
+
+func TestSort(t *testing.T) {
+	got := New(Sort(Less))[0]
+	want := Card{Rank: Ace, Suit: Spade}
+	if got != want {
+		t.Errorf("Expected %s as first card, got %s", want.String(), got.String())
+	}
+}
+
+func TestJokersCount(t *testing.T) {
+	cards := New(Jokers(2))
+	jokers := getJokers(cards)
+
+	if len(jokers) != 2 {
+		t.Errorf("Wrong numbers of Jokers, got %d want 2", len(jokers))
+	}
+}
+
+func TestJokersIdentity(t *testing.T) {
+	cards := New(Jokers(2))
+	jokers := getJokers(cards)
+	m := make(map[Suit]Card)
+	for _, joker := range jokers {
+		m[Suit(joker.Rank)] = joker
+	}
+
+	if len(m) != len(jokers) {
+		t.Errorf("Jokers should be different, got %+v and %+v", jokers[0], jokers[1])
+	}
+}
+
+func getJokers(cards []Card) []Card {
+	jokers := make([]Card, 0, 2)
+	for _, card := range cards {
+		if card.Suit == Joker {
+			jokers = append(jokers, card)
+		}
+	}
+	return jokers
+}
